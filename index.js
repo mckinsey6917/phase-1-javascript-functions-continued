c675194dcfdc01f5250a66aaea46adc4c5a64652
mckinseyleckenby <leckenbym11@gmail.com>
2021-10-23 22:11:00 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+// function razzle() {
+//     console.log("You've been razzled!");
+// }
+// razzle();
+
+// function razzle(lawyer = "Billy", target= "'em") {
+//     console.log (`${lawyer} razzle-dazzles ${target}!`);
+// }
+// razzle();
+// razzle("Methuselah", "T'challah");
+
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork (object = "go to the office") {
+    return (`This Monday, I will ${object}.`)
+}
+
+mondayWork()
+
+function wrapAdjective(style = "*") {
+    return function (adjective = "special") {
+    return `You are ${style}${adjective}${style}!`
+}
+}
